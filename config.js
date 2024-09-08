3196deeeee6ccf7a2ab47031b1153fe6b85235a8
Vishwa503 <dinalvishwa59@gmail.com>
2024-09-08 06:13:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "Vishwa",
+    ownernumber: process.env.Owner_Number || "94763214593",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://vishwa:SznFRoj3x5OvRZNT@vishwa.bzc42.mongodb.net/?retryWrites=true&w=majority&appName=vishwa",
+    sessionId: process.env.SESSION_ID || "a2p3b2U_Queen-Anya_xOW8=",
 
     /**
      * thumbnails and medias
